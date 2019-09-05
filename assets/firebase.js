a45da5fb126cde728b96aa2c8a02f81166754f11
sabsrolz <sabsrolz@umich.edu>
2019-09-05 16:57:51 -0500

    wrote data to db

--- assets/firebase.js
+++ assets/firebase.js
@@ -9,5 +9,53 @@ $(document).ready(function() {
     appId: "1:339788187661:web:7822c8897f00f256936d44"
   };
 
-  firebaseConfig.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
+  //   let trainAdded;
+  //   let destinationAdded;
+  //   let timeAdded;
+  //   let frequencyAdded;
+
+  //declare variables for administrator input
+
+  $("#submit-button").on("click", function() {
+    event.preventDefault();
+    let trainAdded = $("#trainInput")
+      .val()
+      .trim();
+    let destinationAdded = $("#destinationInput")
+      .val()
+      .trim();
+    let timeAdded = $("#timeInput")
+      .val()
+      .trim();
+    let frequencyAdded = $("#frequencyInput")
+      .val()
+      .trim();
+    database.ref().set({
+      train: trainAdded,
+      destination: destinationAdded,
+      time: timeAdded,
+      freq: frequencyAdded
+    });
+  });
+
+  database.ref().on("value", function(snapshot) {
+    //event.preventDefault();
+    console.log(snapshot.val());
+
+    trainAdded = snapshot.val().train;
+    destinationAdded = snapshot.val().destination;
+    timeAdded = snapshot.val().time;
+    frequencyAdded = snapshot.val().freq;
+
+    let rowAdded = `<tr>
+    <th scope="row">${trainAdded}</th>
+    <td>${destinationAdded}</td>
+    <td>${frequencyAdded}</td>
+    <td>New Arrival 1</td>
+    <td>Minutes Away 1</td>
+     </tr>`;
+    $("#train-schedule").append(rowAdded);
+  });
 });
