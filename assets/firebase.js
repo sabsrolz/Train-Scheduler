633195c6a907e33ffd5470f5dd94d5f1f2eb0590
sabsrolz <sabsrolz@umich.edu>
2019-09-05 12:13:33 -0500

    created index file and assets directory

---
+++ assets/firebase.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDYjRkZUQ_uVg_YPdmzJAqCrLSMX083lPo",
+    authDomain: "train-scheduler-f0c2a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-f0c2a.firebaseio.com",
+    projectId: "train-scheduler-f0c2a",
+    storageBucket: "",
+    messagingSenderId: "339788187661",
+    appId: "1:339788187661:web:7822c8897f00f256936d44"
+  };
+
+  firebaseConfig.initializeApp(firebaseConfig);
+});
